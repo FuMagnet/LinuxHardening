41c1956ad486369e4a6515321c5a477fe3f230d2
Stefan <55971452+FuMagnet@users.noreply.github.com>
2021-02-25 15:02:31 +0100

    Update install-f2ban.sh

--- install-f2ban.sh
+++ install-f2ban.sh
@@ -4,6 +4,22 @@
 sudo apt update
 sudo apt-get install fail2ban
 sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+echo >> /etc/fail2ban/jail.local
+echo "[proxmox2]" >> /etc/fail2ban/jail.local
+echo "enabled = true" >> /etc/fail2ban/jail.local
+echo "port    = https,http,8006" >> /etc/fail2ban/jail.local
+echo "filter  = proxmox2" >> /etc/fail2ban/jail.local
+echo "logpath  = /var/log/daemon.log" >> /etc/fail2ban/jail.local
+echo "maxretry = 7" >> /etc/fail2ban/jail.local
+echo "bantime  = 43200" >> /etc/fail2ban/jail.local
+
+touch /etc/fail2ban/filter.d/proxmox2.conf
+
+echo "[Definition]" >> /etc/fail2ban/filter.d/proxmox2.conf
+echo "failregex = pvedaemon\[.*authentication failure; rhost=<HOST> user=.*msg=.*" >> /etc/fail2ban/filter.d/proxmox2.conf
+echo "ignoreregex = " >> /etc/fail2ban/filter.d/proxmox2.conf
+
 sudo systemctl enable fail2ban
 sudo systemctl start fail2banbash
 
